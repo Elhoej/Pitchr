e930fade60b46c317b8e254aa821dfd5b15d3171
Simon Elhoej <simonelhoej@gmail.com>
2019-02-12 14:27:07 +0100

    Initial commit

---
+++ Pitchr/Pitchr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pitchr
+//
+//  Created by Simon Elhoej Steinmejer on 12/02/19.
+//  Copyright © 2019 Simon Elhoej Steinmejer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
