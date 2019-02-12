cae29140a1e3edbc57aaa9b9b492376f0d072493
Simon Elhoej <simonelhoej@gmail.com>
2019-02-12 15:19:47 +0100

    Started setting up some stackviews

--- Pitchr/Pitchr/ViewController.swift
+++ Pitchr/Pitchr/ViewController.swift
@@ -13,6 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        redView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        redView.backgroundColor = .blue
+        
+        let greenView = UIView()
+        redView.backgroundColor = .green
+        greenView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        
+        let topStackView = UIStackView(arrangedSubviews: [])
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let bottomStackView = HomeBottomControlsStackView(arrangedSubviews: [])
+        bottomStackView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let containerStackView = UIStackView(arrangedSubviews: [topStackView, blueView, bottomStackView])
+        containerStackView.axis = .vertical
+        
+        view.addSubview(containerStackView)
+        containerStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
     }
 
 
---
+++ Pitchr/Pitchr/Views/HomeBottomControlsStackView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeBottomControlsStackView.swift
+//  Pitchr
+//
+//  Created by Simon Elhoej Steinmejer on 12/02/19.
+//  Copyright © 2019 Simon Elhoej Steinmejer. All rights reserved.
+//
+
+import UIKit
+
+class HomeBottomControlsStackView: UIStackView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
